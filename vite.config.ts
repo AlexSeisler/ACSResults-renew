6f5a44a697787aacd0f6231c61a41a536f3facf2
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-09-13 17:04:20 -0400

    configid

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',   // 👈 ensures assets resolve correctly in Netlify
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
